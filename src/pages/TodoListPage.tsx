77858d622142e9482973e43099d3dc85c044c747
mura-work <murata.naoya.work@gmail.com>
2023-10-26 22:56:30 +0900

    [fix] フォームをchakraUIに変更

--- src/pages/TodoListPage.tsx
+++ src/pages/TodoListPage.tsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import {
   Button,
-  Text,
-  Checkbox,
   FormControl,
   FormLabel,
   FormErrorMessage,
@@ -10,10 +8,18 @@ import {
   Textarea,
   Input,
   Badge,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
 } from "@chakra-ui/react";
 import api from "../services/api";
 import { TodoType } from "../types/index";
 import { TodoItem } from "components/TodoItem/TodoItem";
+import { SmallAddIcon } from "@chakra-ui/icons";
 
 type TodoForm = {
   title: string;
@@ -30,6 +36,7 @@ export const TodoListPage = () => {
     completedDate: new Date().toLocaleDateString("sv-SE"),
     responsibleUserName: "",
   });
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   useEffect(() => {
     fetchTodoLists();
@@ -60,6 +67,7 @@ export const TodoListPage = () => {
           responsibleUserName: "",
         });
       });
+    setIsOpen(false);
   };
 
   const updateTodoCompleted = async (id: number, checked: boolean) => {
@@ -98,57 +106,83 @@ export const TodoListPage = () => {
             />
           );
         })}
-        <div className="mt-4">
-          <FormControl className="mt-4">
-            <FormLabel>タスク名</FormLabel>
-            <Input
-              type="text"
-              value={todoForm.title}
-              onChange={(e) =>
-                setTodoForm((prev) => ({ ...prev, title: e.target.value }))
-              }
-            />
-          </FormControl>
-          <FormControl className="mt-4">
-            <FormLabel>タスク内容</FormLabel>
-            <Textarea
-              value={todoForm.content}
-              onChange={(e) =>
-                setTodoForm((prev) => ({ ...prev, content: e.target.value }))
-              }
-            />
-          </FormControl>
-          <FormControl className="mt-4">
-            <FormLabel>期限日</FormLabel>
-            <Input
-              type="date"
-              value={todoForm.completedDate}
-              onChange={(e) =>
-                setTodoForm((prev) => ({
-                  ...prev,
-                  completedDate: e.target.value,
-                }))
-              }
-            />
-          </FormControl>
-          {/* いずれユーザー登録機能を作り、ユーザーをプルダウンで表示させたい */}
-          <FormControl className="mt-4">
-            <FormLabel>担当者</FormLabel>
-            <Input
-              type="text"
-              value={todoForm.responsibleUserName}
-              onChange={(e) =>
-                setTodoForm((prev) => ({
-                  ...prev,
-                  responsibleUserName: e.target.value,
-                }))
-              }
-            />
-          </FormControl>
-          <Button className="mt-4" onClick={postTodo}>
-            タスク追加
-          </Button>
-        </div>
+        <Button className="rounded-full" onClick={() => setIsOpen(true)}>
+          <SmallAddIcon className="rounded-full" />
+        </Button>
+        <Modal
+          isOpen={isOpen}
+          onClose={() => setIsOpen(false)}
+        >
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Todo Form</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <div className="mt-4">
+                <FormControl className="mt-4">
+                  <FormLabel>タスク名</FormLabel>
+                  <Input
+                    type="text"
+                    value={todoForm.title}
+                    onChange={(e) =>
+                      setTodoForm((prev) => ({
+                        ...prev,
+                        title: e.target.value,
+                      }))
+                    }
+                  />
+                </FormControl>
+                <FormControl className="mt-4">
+                  <FormLabel>タスク内容</FormLabel>
+                  <Textarea
+                    value={todoForm.content}
+                    onChange={(e) =>
+                      setTodoForm((prev) => ({
+                        ...prev,
+                        content: e.target.value,
+                      }))
+                    }
+                  />
+                </FormControl>
+                <FormControl className="mt-4">
+                  <FormLabel>期限日</FormLabel>
+                  <Input
+                    type="date"
+                    value={todoForm.completedDate}
+                    onChange={(e) =>
+                      setTodoForm((prev) => ({
+                        ...prev,
+                        completedDate: e.target.value,
+                      }))
+                    }
+                  />
+                </FormControl>
+                {/* いずれユーザー登録機能を作り、ユーザーをプルダウンで表示させたい */}
+                <FormControl className="mt-4">
+                  <FormLabel>担当者</FormLabel>
+                  <Input
+                    type="text"
+                    value={todoForm.responsibleUserName}
+                    onChange={(e) =>
+                      setTodoForm((prev) => ({
+                        ...prev,
+                        responsibleUserName: e.target.value,
+                      }))
+                    }
+                  />
+                </FormControl>
+              </div>
+            </ModalBody>
+            <ModalFooter>
+              <Button colorScheme="blue" mr={3} onClick={postTodo}>
+                タスク追加
+              </Button>
+              <Button onClick={() => setIsOpen(false)} variant="ghost">
+                キャンセル
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
       </div>
     </div>
   );
